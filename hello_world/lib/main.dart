fafad75986b6230e75c1ab8a449ec1a27d4b8519
BK <benjamin@wonder4studio.com>
2023-05-07 22:31:15 +0800

    feat: hello world.

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+//刪除註解
+// ctrl + R，輸入/*([\S\s]+?)，
+// 選取 Regex 正規式搜尋
+// 將Filter勾選成 In Comments and Replace all。
+// 最後用ctrl + alt + L排版程式碼。
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pushed the button this times:$_counter',
+              style: const TextStyle(
+                  fontSize: 25,
+                  color: Colors.green,
+                  fontStyle: FontStyle.italic),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
